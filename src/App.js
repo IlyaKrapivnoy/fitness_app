be5f7927ba8597ea6c3120f24c33dbcfd89133fe
KrapivnoyI <ilya.krapivnoy@scelex.com>
2022-06-13 22:56:06 +0300

    add pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
