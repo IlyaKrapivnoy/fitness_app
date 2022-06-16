3c1e765b597ee14c7ff680f0fd18af13bf3e2202
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-06-16 19:44:24 +0300

    add Typography

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        ml: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#ff2625" fontWeight={600} fontSize="25px">
+        Fitness Club
+      </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+      >
+        Sweat, Smile
+        <br />
+        and Repeat
+      </Typography>
+      <Typography fontSize="22px" lineHeight="35px">
+        Check out the most effective exercises
+      </Typography>
+    </Box>
+  );
 };
 
 export default HeroBanner;
