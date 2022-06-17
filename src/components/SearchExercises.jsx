96279543c348764c4cc15d3daac08d4de6a90dbf
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-06-17 18:32:22 +0300

    add btn

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 const SearchExercises = () => {
@@ -7,25 +7,40 @@ const SearchExercises = () => {
       <Typography
         fontWeight={700}
         sx={{ fontSize: { lg: "44px", xs: "30px" } }}
-        mb="50px"
+        mb="49px"
         textAlign="center"
       >
-        Awesome Exercises You
-        <br /> Should Know
+        Awesome Exercises You <br /> Should Know
       </Typography>
       <Box position="relative" mb="72px">
         <TextField
+          height="76px"
           sx={{
             input: { fontWeight: "700", border: "none", borderRadius: "4px" },
             width: { lg: "1170px", xs: "350px" },
             backgroundColor: "#fff",
             borderRadius: "40px",
           }}
-          height="76px"
-          value=""
-          onChange={(e) => {}}
+          onChange={null}
           placeholder="Search Exercises"
+          type="text"
         />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "173px", xs: "80px" },
+            height: "56px",
+            position: "absolute",
+            right: "0px",
+            fontSize: { lg: "20px", xs: "14px" },
+          }}
+          onClick={null}
+        >
+          Search
+        </Button>
       </Box>
     </Stack>
   );
